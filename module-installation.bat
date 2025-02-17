c58d0b0a6bcc69dd7fe5569558f5c0c1d3f83e6c
Aket0r <57960431+aket0r@users.noreply.github.com>
2025-02-17 23:01:41 +0100

    Update module-installation.bat

--- module-installation.bat
+++ module-installation.bat
@@ -1,1 +1,1 @@
-npm i open-file-text-editor && npm i fs && npm i node-processlist && npm i child_process && npm i url && npm i path && npm i electron && npm i electron-packager && npm i os
+npm init && npm i open-file-text-editor && npm i fs && npm i node-processlist && npm i child_process && npm i url && npm i path && npm i electron && npm i electron-packager && npm i os
