6da2466d3f94caa3aad8ddd2eb2d9822d31ff09d
Aket0r <zerotwozefrankxx@gmail.com>
2025-02-18 03:59:41 +0100

    fixed fixing bugs and improving missing functions

--- module-installation.bat
+++ module-installation.bat
@@ -1,1 +1,1 @@
-npm init && npm i open-file-text-editor && npm i fs && npm i node-processlist && npm i child_process && npm i url && npm i path && npm i electron && npm i electron-packager && npm i os
+npm i open-file-text-editor && npm i fs && npm i node-processlist && npm i child_process && npm i url && npm i path && npm i electron && npm i electron-packager && npm i os
